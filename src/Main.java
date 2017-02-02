12af490a061a911aff4c19499bc3b8a0d3821bf8
imsch <jannis.schmitt92@gmail.com>
2017-02-02 13:48:39 +0100

    clean master branch

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by legion on 12/6/16.
- */
-public class Main {
-    public static void main(String[] args){
-        System.out.print("Hello World");
-    }
-}
--- src/Numbers/Number.java
+++
@@ -1,7 +0,0 @@
-package Numbers;
-/**
- * Created by legion on 11/4/16.
- */
-public abstract class Number {
-    public abstract String toString();
-}
